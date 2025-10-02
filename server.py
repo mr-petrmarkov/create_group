cfa11c8b64c5a57ce0adeb589361bb65668e2168
mr-petrmarkov <petr.markov@ognex.com>
2025-10-02 17:46:13 +0300

    Initial commit: Telethon + Flask server

--- server.py
+++ server.py
@@ -6,6 +6,8 @@ api_id = 21334519
 api_hash = "ad90b94b00185c6d9b0341af99121cf2"
 
 client = TelegramClient("my_session", api_id, api_hash)
+client.start()  # запускаем сразу при старте приложения
+
 app = Flask(__name__)
 
 @app.route("/create_group", methods=["POST"])
@@ -20,8 +22,6 @@ def create_group():
             title=title
         ))
 
-    with client:
-        result = client.loop.run_until_complete(runner())
-
+    result = client.loop.run_until_complete(runner())
     return jsonify({"status": "ok", "group": str(result)})
 
